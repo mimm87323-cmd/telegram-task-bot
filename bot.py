93699cafff8fdd233e640f68a4337e32afe6a7af
mimm87323-cmd <mimm87323@gmail.com>
2026-08-27 09:41:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
+import os
+from threading import Thread
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8869494633:AAFTi9UySlNxXkjDCY7NR81dFHT4-2EoYP0"
+TOKEN = "8869494633:AAFTi9UySlNxXkjDCY7NR81dFHT4-2EoYP0" # আপনার মূল টোকেনটি কোটেশনের ভেতরে বসাবেন
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "🤖 স্বাগতম!\n\n"
+        "🤖 স্বাগত!\n\n"
         "আমাদের Task Bot শিগগিরই প্রস্তুত হচ্ছে।"
     )
 
@@ -15,5 +18,22 @@ app = (
 app.add_handler(CommandHandler("start", start))
 
 print("Bot is running...")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+
+    class Handler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Bot is running")
+
+        def log_message(self, format, *args):
+            pass
+
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+Thread(target=run_server, daemon=True).start()
 
 app.run_polling()
